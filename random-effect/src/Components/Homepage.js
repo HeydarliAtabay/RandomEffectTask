6fec24aba9bbf7fd1e3a95cdf951e828e9127960
HeydarliAtabay <heydarli.atabay@gmail.com>
2022-04-25 22:56:17 +0200

    Design issues were fixed

--- random-effect/src/Components/Homepage.js
+++ random-effect/src/Components/Homepage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row,Col } from "react-bootstrap";
+import { Container, Row,Col, Image, Figure, Form } from "react-bootstrap";
 import { FileUploader } from "react-drag-drop-files";
 
 
@@ -8,31 +8,67 @@ const fileTypes = ["JPG", "PNG", "GIF"];
 function Homepage(props) {
 
   const [file, setFile] = useState(null);
+  const [height, setHeight] = useState(600);
+  const [width, setWidth] = useState(300);
 
   const handleChange = (file) => {
     setFile(file);
   };
   return (
 <>
+<Container fluid>
 <div className="cont">
 
-<FileUploader handleChange={handleChange} name="file" types={fileTypes} />
-{(file !==null)&& <ImageBox file={file.name} />}
-
-</div>
-
+<div className="drag"><FileUploader handleChange={handleChange} name="file" types={fileTypes}  hoverTitle="Drop the image here" style={{width:"500px"}} />
+{(file !==null)&&
+<>
+<ImageBox file={file.name} height={height} width={width} />
+<Row>
+  <Col sm={1}>Size</Col> 
+  <Col sm={3}>
+  <Form.Control
+                type="number"
+                name="width"
+                value={width}
+                onChange={(ev) => setWidth(ev.target.value)}
+                autoFocus
+              />
+  </Col> <Col sm={1}>x</Col>
+  <Col sm={3}>
+  <Form.Control
+                type="number"
+                name="height"
+                value={height}
+                onChange={(ev) => setHeight(ev.target.value)}
+                autoFocus
+              />
+  </Col> 
+</Row>
 </>
-
+}
+</div>
+</div>
+</Container>
+</>
   );
   }
 
 
   function ImageBox(props){
-    const {file}=props
+    const {file, height, width}=props
     return(
       <>
       <div className="imgbx">
-      <img src={require(`../assets/${file}`)} alt="uploaded" height="300" width="300"></img>
+<Figure>
+  <Figure.Image
+    width={parseInt(width)}
+    height={parseInt(height)}
+    src={require(`../assets/${file}`)}
+  />
+    <Figure.Caption>
+      {`${file} was uploaded, please select the filter or transmission`}
+    </Figure.Caption>
+</Figure>
       </div>
       </>
     )
